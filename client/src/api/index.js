ab2297a64997b328f52d8f57a7c8e1bc3ad13707
Matin Sadeghi <matinsadegi3290@gmail.com>
2022-08-30 20:09:51 +0430

    change local server to heroku server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-// const API = axios.create({
-//   baseURL: 'https://ticket-app-mern.herokuapp.com',
-// });
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+  baseURL: 'https://ticket-app-mern.herokuapp.com',
+});
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('employeeProfile')){
